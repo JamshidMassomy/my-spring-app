d7274894f40216e6448c068d97bde3b372fa2b06
jamshid massomy <jamshidmassomi@gmail.com>
2023-07-20 15:50:06 +0300

    clean up

--- api_reactive_version/build.gradle.kts
+++ api_reactive_version/build.gradle.kts
@@ -36,12 +36,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
 
-//	io.r2dbc:r2dbc-postgresql
-//	org.postgresql:postgresql
 
 	compileOnly("org.projectlombok:lombok")
-	// runtimeOnly("com.h2database:h2")
-
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
